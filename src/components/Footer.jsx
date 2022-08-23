749a496b0fd6f936f35404310e04063eb2cec710
smit95tpatel <smit.patel@razorpay.com>
2022-08-23 15:31:12 +0530

    feat: made footer resposive and added key to social list

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Box, Container, HStack, Icon, Link, Text } from '@chakra-ui/react';
+import { Box, Container, Flex, HStack, Icon, Link, Text } from '@chakra-ui/react';
 import { socialLinks } from '../common/components/Navbar';
 
 function Footer() {
 	return (
 		<Box backgroundColor="chakra-footer-bg">
-			<Container display="flex" justifyContent="space-between" alignItems="center" py="6">
-				<HStack justifyContent="space-between" w="100%">
+			<Container py="6">
+				<Flex
+					display="flex"
+					justifyContent="space-between"
+					w="100%"
+					flexWrap="wrap"
+					gap="2">
 					<Text>&copy; 2022 Adarsh Shah. All rights reserved.</Text>
-					<Box mt="0">
-						{socialLinks.map(({ url, icon }) => (
-							<Link href={url} color="chakra-nav-link" mr="4">
+					<HStack>
+						{socialLinks.map(({ url, icon }, index) => (
+							<Link key={index} href={url} color="chakra-nav-link" display="inline-block">
 								<Icon as={icon} verticalAlign="middle" fontSize="2xl" />
 							</Link>
 						))}
-					</Box>
-				</HStack>
+					</HStack>
+				</Flex>
 			</Container>
 		</Box>
 	);
