ec949e9b0974777799d84121e61b5e925c99118b
smit95tpatel <smit.patel@razorpay.com>
2022-10-19 22:26:35 +0530

    fix: retry comp style

--- src/components/projects/Retry.js
+++ src/components/projects/Retry.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { Button, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 import { RepeatIcon, WarningTwoIcon } from '@chakra-ui/icons';
 
 function Retry({ isLoading, refetch }) {
 	return (
 		<>
 			{!isLoading && (
-				<Text display="flex" alignItems="center" gap="1.5">
-					<WarningTwoIcon color="red.500" /> Opps! Something went wrong. Please try
-					refresing{' '}
-					<Button
-						variant="ghost"
-						height="fit-content"
-						p="0"
-						minWidth="3"
-						colorScheme="white"
-						type="button"
-						onClick={refetch}>
-						<RepeatIcon color="blue.500" />
-					</Button>
+				<Text
+					display="flex"
+					alignItems="center"
+					gap="1.5"
+					flexDirection={{ base: 'column', md: 'row' }}>
+					<WarningTwoIcon color="red.500" />
+					<Box textAlign="center">
+						Opps! Something went wrong. Please try refresing{' '}
+						<Button
+							variant="ghost"
+							height="fit-content"
+							p="0"
+							minWidth="3"
+							colorScheme="white"
+							type="button"
+							onClick={refetch}>
+							<RepeatIcon color="blue.500" />.
+						</Button>
+					</Box>
 				</Text>
 			)}
 		</>
